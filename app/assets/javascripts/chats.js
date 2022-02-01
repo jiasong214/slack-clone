608b9a7f9be4d27365a8997bbad8afe819daf97d
Jia Song <jiasong214@gmail.com>
2022-02-01 11:31:19 +1100

    comment out js file for deploy test

--- app/assets/javascripts/chats.js
+++ app/assets/javascripts/chats.js
@@ -1,51 +1,51 @@
-window.onload = () => {
-  // chats list functions -----------------------------------------
-  const chatScrollContainer = document.querySelector(".chats__list-container");
-  const chatList = document.querySelector(".chats__list");
-  chatScrollContainer.scrollTo(0, chatList.offsetHeight);
+// window.onload = () => {
+//   // chats list functions -----------------------------------------
+//   const chatScrollContainer = document.querySelector(".chats__list-container");
+//   const chatList = document.querySelector(".chats__list");
+//   chatScrollContainer.scrollTo(0, chatList.offsetHeight);
 
 
-  // chats form functions -----------------------------------------
-  const chatForm = document.querySelector(".chats__form");
-  const input = document.querySelector(".chats__form__input");
+//   // chats form functions -----------------------------------------
+//   const chatForm = document.querySelector(".chats__form");
+//   const input = document.querySelector(".chats__form__input");
 
-  // text editor setting
-  // - change enter event to submit event
-  // - replace to shift + enter for line change
-  const quill = new Quill('#editor', {
-    theme: 'snow',
-    placeholder: 'Compose an epic...',
-    modules: {
-      toolbar: ['bold', 'italic', 'strike', 'code-block'],
-      keyboard: {
-        bindings: {
-          shift_enter: {
-            key: 13,
-            shiftKey: true,
-            handler: (range, ctx) => {
-              // console.log(range, ctx);
-              editor.insertText(range.index, '\n');
-            }
-          },
-          enter: {
-            key: 13,
-            handler: () => {
-              input.value = document.querySelector(".ql-editor").innerHTML;
-              chatForm.submit();
-            }
-          }
-        }
-      }
-    }
-  });
+//   // text editor setting
+//   // - change enter event to submit event
+//   // - replace to shift + enter for line change
+//   const quill = new Quill('#editor', {
+//     theme: 'snow',
+//     placeholder: 'Compose an epic...',
+//     modules: {
+//       toolbar: ['bold', 'italic', 'strike', 'code-block'],
+//       keyboard: {
+//         bindings: {
+//           shift_enter: {
+//             key: 13,
+//             shiftKey: true,
+//             handler: (range, ctx) => {
+//               // console.log(range, ctx);
+//               editor.insertText(range.index, '\n');
+//             }
+//           },
+//           enter: {
+//             key: 13,
+//             handler: () => {
+//               input.value = document.querySelector(".ql-editor").innerHTML;
+//               chatForm.submit();
+//             }
+//           }
+//         }
+//       }
+//     }
+//   });
 
-  quill.focus();
+//   quill.focus();
 
-  // fill the text with input value when a user click edit chat
-  document.querySelector(".ql-editor").innerHTML = input.value;
+//   // fill the text with input value when a user click edit chat
+//   document.querySelector(".ql-editor").innerHTML = input.value;
 
-  // send a text contents as a value when form is submitted
-  chatForm.addEventListener("submit", function(e){
-    input.value = document.querySelector(".ql-editor").innerHTML;
-  });
-}
+//   // send a text contents as a value when form is submitted
+//   chatForm.addEventListener("submit", function(e){
+//     input.value = document.querySelector(".ql-editor").innerHTML;
+//   });
+// }
